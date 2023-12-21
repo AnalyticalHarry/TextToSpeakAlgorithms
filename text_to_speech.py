61b5cc13e673c1c354622d27b3914745faac5b0a
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2023-12-21 21:58:34 +0000

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,35 +1,32 @@
 import pyttsx3
 
 def speech(text):
-        engine = pyttsx3.init() 
+    engine = pyttsx3.init()
 
-        """ RATE"""
-        #current speaking rate
-        rate = engine.getProperty('rate')   
-        print()
-        #printing current voice rate
-        print(f"Speed rate of speech: {rate}") 
-        #setting up new voice rate
-        engine.setProperty('rate', 180)     
+    """ RATE"""
+    #the current speaking rate
+    rate = engine.getProperty('rate')
+    print(f"Speed rate of speech: {rate}")
+    #a new voice rate (adjust as needed)
+    engine.setProperty('rate', 180)
 
+    """VOLUME"""
+    #the current volume level (min=0 and max=1)
+    volume = engine.getProperty('volume')
+    print(f"Volume of speech: {volume}")
+    # Set the volume level between 0 and 1 (adjust as needed)
+    engine.setProperty('volume', 1.0)
 
-        """VOLUME"""
-        #getting to know current volume level (min=0 and max=1)
-        volume = engine.getProperty('volume')   
-        print()
-        #printing current volume level
-        print(f"Volume of speech: {volume}")             
-        #setting up volume level  between 0 and 1
-        engine.setProperty('volume',1.0)    
-        engine.say(text)
-        engine.runAndWait()
-        engine.stop()
+    #say the text
+    engine.say(text)
+    engine.runAndWait()
 
-        """Saving Voice to a file"""
-        engine.save_to_file(text, 'test.mp3')
-        engine.runAndWait()
-    
-text = ''' Machine learning is a subfield of artificial intelligence, 
+    """Saving Voice to a file"""
+    engine.save_to_file(text, 'test.mp3')
+    engine.runAndWait()
+    engine.stop()
+
+text = '''Machine learning is a subfield of artificial intelligence, 
 which is broadly defined as the capability of a machine to imitate intelligent human behavior. 
 Artificial intelligence systems are used to perform complex tasks in a way that is similar to how humans solve problems.'''
 speech(text)
